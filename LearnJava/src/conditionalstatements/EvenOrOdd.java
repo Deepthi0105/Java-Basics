b35f0d9f17544372da670e26041c70fbbb14cf00
deept <deept@10.0.0.169>
2021-08-11 00:11:55 -0500

    More exercises on Conditional Statements

---
+++ LearnJava/src/conditionalstatements/EvenOrOdd.java
@@ -0,0 +1,19 @@
+package conditionalstatements;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+
+		int number = 16;
+
+		if (number % 2 == 0)
+
+		{
+			System.out.println("Even Number");
+		} else 
+		{
+			System.out.println("Odd number");
+		}
+	}
+
+}
---
+++ LearnJava/src/conditionalstatements/Swapping.java
@@ -0,0 +1,23 @@
+package conditionalstatements;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+
+		System.out.println("Value of a before swapping" + a);
+		System.out.println("Value of b before swapping" + b);
+
+		//int c;
+
+		//c = a;
+		a = b;
+		b = a;
+
+		System.out.println("value of a after swapping" + a);
+		System.out.println("value of b after swapping" + b);
+	}
+
+}
