499eb6e486eb2c5eec7f8fde85ed24d0aa5df348
deept <deept@10.0.0.169>
2021-08-11 00:15:00 -0500

    While, do\while, exercise

---
+++ LearnJava/src/loops/WhileLoopEg1.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class WhileLoopEg1 {
+	public static void main(String[] args) {
+
+		int i = 0;
+
+		while (i <= 20) {
+			System.out.println(i);
+			i += 2;
+		}
+
+		System.out.println("Program exited");
+	}
+
+}
